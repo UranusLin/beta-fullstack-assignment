1ae0ce64f8fd938bb793b0022b9b31c93314900e
MorrisLin <aka611511@gmail.com>
2023-05-28 18:13:21 +0800

    impl data_collection & arbitrage_strategy & trading_bot (#2)
    * feat: update solution pdf
    feat: init design system implement via Rust
    
    * change edition to 2023
    feat: change edition to 2023
    
    * impl data collection module
    feat: impl sample fetch data from exchange
    feat: fix Cargo.toml edition error
    
    * update solution pdf and impl redis pub sub
    feat: update solution pdf
    feat: data_collection impl redis pub
    BREAKING-CHANGE: remove event_dispatcher
    
    * Refactor: Move Exchange struct to trading_bot_lib
    feat: This commit moves the Exchange struct from the data_collection package to the trading_bot_lib package. The aim is to make the Exchange struct reusable across different modules. This change promotes code reusability and modularity, keeping the Exchange related logic in a centralized location.
    
    * impl arbitrage_strategy & trading_bot
    feat: impl arbitrage_strategy sub from data collection and make decision pub to redis
    feat: impl trading_bot sub from arbitrage_decision to call buy / sell api and pub result

---
+++ system_design/trading_bot_system/packages/data_collection/src/lib.rs
@@ -0,0 +1,76 @@
+// use redis::ErrorKind;
+use reqwest;
+use std::future::Future;
+use core::pin::Pin;
+use redis::Commands;
+use redis::Client;
+use trading_bot_lib::Exchange;
+
+pub trait BitcoinPriceGetter {
+    fn get_bitcoin_price(&self, timestamp: i64) -> Pin<Box<dyn Future<Output = Result<f64, reqwest::Error>> + Send + '_>>;
+}
+
+pub fn publish_price_to_redis(exchange_name: &str, price: f64) -> Result<(), redis::RedisError> {
+    // Create a client
+    let client = Client::open("redis://127.0.0.1/")?;
+
+    // Connect to Redis
+    let mut con = client.get_connection()?;
+
+    // Construct the channel name from the exchange name
+    let channel_name = format!("{}_price", exchange_name);
+
+    // Publish the price data to the channel
+    con.publish(&channel_name, price.to_string())?;
+
+    Ok(())
+}
+
+impl BitcoinPriceGetter for Exchange {
+    fn get_bitcoin_price(&self, timestamp: i64) -> Pin<Box<dyn Future<Output = Result<f64, reqwest::Error>> + Send + '_>> {
+        Box::pin(async move {
+            let client = reqwest::Client::new();
+            let endpoint = format!("{}/price_bitcoin?timestamp={}", self.api_url, timestamp);
+            let response = client.get(&endpoint).send().await?;
+
+            // Parse the response into a f64 price
+            let price: f64 = response.text().await?.parse().unwrap();
+
+            // Publish the price to Redis
+            match publish_price_to_redis(&self.name, price) {
+                Ok(_) => println!("Price data from {} published to Redis successfully", self.name),
+                Err(err) => eprintln!("Failed to publish price data from {} to Redis: {}", self.name, err),
+            };
+
+            Ok(price)
+        })
+    }
+}
+
+struct MockExchange {
+    #[allow(dead_code)]
+    pub name: String,
+}
+
+impl BitcoinPriceGetter for MockExchange {
+    fn get_bitcoin_price(&self, _timestamp: i64) -> Pin<Box<dyn Future<Output = Result<f64, reqwest::Error>> + Send>> {
+        Box::pin(async { Ok(50000.0) })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::executor::block_on;
+
+    #[test]
+    fn test_get_bitcoin_price() {
+        let mock_exchange = MockExchange { name: "MockExchange".to_string() };
+
+        // Now we can call get_bitcoin_price on the mock object
+        let price = block_on(mock_exchange.get_bitcoin_price(1619827200)).unwrap();
+
+        // And we know that it should return the fixed price we specified
+        assert_eq!(price, 50000.0);
+    }
+}
--- system_design/trading_bot_system/packages/trading_bot_lib/src/lib.rs
+++ system_design/trading_bot_system/packages/trading_bot_lib/src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Exchange {
+    pub name: String,
+    pub api_url: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Serialize, Deserialize)]
+pub struct TradeDecision {
+    exchange_name: String,
+    action: String, // "buy" or "sell"
+    price: f64,
+    quantity: f64,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PriceUpdate {
+    pub exchange_name: String,
+    pub price: f64,
+}
+
+
+impl Exchange {
+    pub fn new(name: String, api_url: String) -> Exchange {
+        Exchange { name, api_url }
+    }
+}
+
+impl TradeDecision {
+    pub fn new(exchange_name: String, action: String, price: f64, quantity: f64) -> Self {
+        TradeDecision {
+            exchange_name,
+            action,
+            price,
+            quantity,
+        }
+    }
+    pub fn exchange_name(&self) -> &String {
+        &self.exchange_name
+    }
+
+    pub fn action(&self) -> &String {
+        &self.action
+    }
+
+    pub fn price(&self) -> f64 {
+        self.price
+    }
+
+    pub fn quantity(&self) -> f64 {
+        self.quantity
+    }
+}
+
+impl PriceUpdate {
+    pub fn new(exchange_name: String, price: f64) -> PriceUpdate {
+        PriceUpdate { exchange_name, price }
     }
 }
