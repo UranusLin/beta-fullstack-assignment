347199013f3b2dcd39e398e17103aaff85910c6d
MorrisLin <aka611511@gmail.com>
2023-05-29 01:51:30 +0800

    add navBack & title component feat: add navBack & title component but have some bug

---
+++ simple_application/static-page-impl/src/components/title.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Title() {
+    return (
+        <div className="mt-5 relative">
+            <h1 className="absolute left-0 top-0 font-semibold text-xl leading-8 text-custom-color">
+                Lorem ipsum dolor sit amet
+            </h1>
+            <p className="absolute left-336 top-5 font-normal text-xs leading-5 text-white">
+                Active
+            </p>
+            <p className="absolute left-0 top-43 font-normal text-xs leading-5 text-custom-color">
+                Active ends in : 2 days
+            </p>
+        </div>
+    )
+}
--- simple_application/static-page-impl/tailwind.config.js
+++ simple_application/static-page-impl/tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'custom-color': '#2A2B38',
       },
+      spacing: {
+        '120': '7.5rem',
+      }
     },
   },
   plugins: [],
